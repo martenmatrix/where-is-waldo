579a69fda8165be521aed7ee66151bfeae87fc7a
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-05-23 01:14:05 +0200

    Add SearchImage component and method to get relative coordinates when clicking image

---
+++ src/components/SearchImage.jsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+import mainImage from '../img/search-image.jpg';
+import { useRef } from 'react';
+// TODO include TargetBox here and add a function prop to send a request
+
+const Image = styled.img.attrs({ src: mainImage})`
+    display: block;
+    width: 100%;
+    object-fit: contain;
+`;
+
+function SearchImage() {
+    const ImageRef = useRef();
+
+    function getCoordinates(e) {
+        const round = (num) => Math.round(num);
+
+        const image = ImageRef.current;
+        const rect = image.getBoundingClientRect();
+
+        // pixels of the original image / pixels of the current window => produces a scale value (you could use any other static number for the naturalWidth)
+        const widthScale = image.naturalWidth / image.clientWidth;
+        const heightScale = image.naturalHeight / image.clientHeight;
+
+        // current pixel in the viewport (if scrolled down, top left will still result in 0) / distance from element from the edge of the viewport
+        const relativeX = e.clientX - rect.left;
+        const relativeY = e.clientY - rect.top;
+
+        const x = round(relativeX * widthScale);
+        const y = round(relativeY * heightScale);
+
+        return { x, y }
+    }
+
+    function onClick(event) {
+        const coordinates = getCoordinates(event);
+        console.log(coordinates);
+    }
+
+    return (
+            <Image ref={ImageRef} onClick={onClick} />
+    )
+}
+
+export default SearchImage;
