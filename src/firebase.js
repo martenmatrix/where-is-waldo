2d1ec3be4eff8100eab22fe6265086a0e5b4e689
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-05-22 18:57:13 +0200

    Add firebase hosting and realtime database

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB9BJBVa2UtjarhtFJ9KSRlPvrg6G9QqOM",
+  authDomain: "where-is-waldo-e61b2.firebaseapp.com",
+  projectId: "where-is-waldo-e61b2",
+  storageBucket: "where-is-waldo-e61b2.appspot.com",
+  messagingSenderId: "1011447303942",
+  appId: "1:1011447303942:web:4c4697da1d49f13ff0d887"
+};
+
+const app = initializeApp(firebaseConfig);
