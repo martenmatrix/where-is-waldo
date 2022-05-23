008bbecde7b6e5e8cc6c190da17a7ea1e6374cf7
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-05-23 17:15:14 +0200

    Hide overflow

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,25 @@
 import Modal from './Modal';
 import Header from './Header';
+import SearchImage from './SearchImage';
+import Game from '../game';
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
     body {
         margin: 0;
         padding: 0;
+        overflow-x: hidden;
     }
 `
 
 export default function App() {
+    const characters = Game.getCharacters();
+
     return (
         <>
             <GlobalStyle />
             <Header time={'5:11'}/>
+            <SearchImage charactersToFind={characters}/>
             <Modal isActive={false}/>
         </>
     )
