38cd1f68545c7b213bc91a0e875c50a2f87f42f3
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-05-23 13:49:38 +0200

    Include TargetBox component and divide coordinates into relative and absolute

--- src/components/SearchImage.jsx
+++ src/components/SearchImage.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import mainImage from '../img/search-image.jpg';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import TargetBox from './TargetBox';
 // TODO include TargetBox here and add a function prop to send a request
 
 const Container = styled.div`
@@ -13,10 +14,13 @@ const Image = styled.img.attrs({ src: mainImage})`
     width: 100%;
     object-fit: contain;
     pointer-events: auto;
+    margin-top: 5rem;
 `;
 
-function SearchImage() {
+function SearchImage({ charactersToFind }) {
     const ImageRef = useRef();
+    const [absoluteCoordinates, setAbsoluteCoordinates] = useState();
+    const [relativeCoordinates, setRelativeCoordinates] = useState();
 
     function getCoordinates(e) {
         const round = (num) => Math.round(num);
@@ -24,7 +28,7 @@ function SearchImage() {
         const image = ImageRef.current;
         const rect = image.getBoundingClientRect();
 
-        // pixels of the original image / pixels of the current window => produces a scale value (you could use any other static number for the naturalWidth)
+        // pixels of the original image / pixels of the current window => produces a scale value (you could use any other static number for the naturalWidth, however then you would not have the exact pixel for css styling)
         const widthScale = image.naturalWidth / image.clientWidth;
         const heightScale = image.naturalHeight / image.clientHeight;
 
@@ -32,20 +36,21 @@ function SearchImage() {
         const relativeX = e.clientX - rect.left;
         const relativeY = e.clientY - rect.top;
 
-        const x = round(relativeX * widthScale);
-        const y = round(relativeY * heightScale);
+        const absoluteX = round(relativeX * widthScale);
+        const absoluteY = round(relativeY * heightScale);
 
-        return { x, y }
+        setAbsoluteCoordinates({ x: absoluteX, y: absoluteY });
+        setRelativeCoordinates({ x: relativeX, y: relativeY });
     }
 
     function onClick(event) {
-        const coordinates = getCoordinates(event);
-        console.log(coordinates);
+        getCoordinates(event);
     }
 
     return (
         <Container>
             <Image ref={ImageRef} onClick={onClick} />
+            {relativeCoordinates && <TargetBox x={relativeCoordinates.x} y={relativeCoordinates.y} options={charactersToFind}/>}
         </Container>
     )
 }
