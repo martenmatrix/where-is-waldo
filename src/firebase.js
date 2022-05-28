d2dfc365373582844f1da52834a90f80b84c0b03
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-05-28 18:46:24 +0200

    Add function to get correct coordinates for a character id

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,37 @@
 import { initializeApp } from "firebase/app";
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase, ref, get, onValue } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB9BJBVa2UtjarhtFJ9KSRlPvrg6G9QqOM",
-  authDomain: "where-is-waldo-e61b2.firebaseapp.com",
-  projectId: "where-is-waldo-e61b2",
-  storageBucket: "where-is-waldo-e61b2.appspot.com",
-  messagingSenderId: "1011447303942",
-  appId: "1:1011447303942:web:4c4697da1d49f13ff0d887"
+    apiKey: "AIzaSyB9BJBVa2UtjarhtFJ9KSRlPvrg6G9QqOM",
+    authDomain: "where-is-waldo-e61b2.firebaseapp.com",
+    databaseURL: 'https://where-is-waldo-e61b2-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: "where-is-waldo-e61b2",
+    storageBucket: "where-is-waldo-e61b2.appspot.com",
+    messagingSenderId: "1011447303942",
+    appId: "1:1011447303942:web:4c4697da1d49f13ff0d887"
 };
 
 const app = initializeApp(firebaseConfig);
+
+const databaseHandler = (function () {
+    const database = getDatabase(app);
+
+    async function getCoordinatesFor(characterId) {
+        const charactersSolutionsReference = ref(database, `solutions/${characterId}`);
+        const coordinatesSnapshot = await get(charactersSolutionsReference);
+        const coordinates = coordinatesSnapshot.val();
+        return coordinates;
+    }
+
+    function createHighScoreListener(onChange) {
+
+    }
+
+    function uploadHighScore(username, time) {
+        
+    }
+
+    return { getCoordinatesFor, getHighscores, uploadHighScore  }
+})();
+
+export { databaseHandler }; 
