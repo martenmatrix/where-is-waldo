85de1ddc5fd682429ce439eefe4d53346ae5d02a
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-05-30 01:46:14 +0200

    Add react-alert component to root

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import { Provider as AlertProvider } from '@blaumaus/react-alert';
+import AlertTemplate from 'react-alert-template-basic';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <AlertProvider template={AlertTemplate}>
+            <App />
+        </AlertProvider>
     </React.StrictMode>
 );
