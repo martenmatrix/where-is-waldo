3a25421c8fc24e1250b661d87de89dca788ff512
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-05-30 17:25:09 +0200

    Import bootstrap

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { positions, Provider as AlertProvider } from '@blaumaus/react-alert';
 import AlertTemplate from 'react-alert-template-basic';
 
