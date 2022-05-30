1127610ba19f6f264263578fbc444e83a2f0d395
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-05-30 17:35:34 +0200

    Add image of characters to with game explanation and use Bootstrap for the button

--- src/components/StartGameModal.jsx
+++ src/components/StartGameModal.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import CharactersImage from '../img/characters-to-find.png';
 import Modal from './Modal';
 import { Button } from 'reactstrap';
 
@@ -7,14 +8,30 @@ const Container = styled.div`
     height: 100%;
 
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
+`
+
+const CharactersToFindImage = styled.img.attrs({ src: CharactersImage })`
+    display: block;
+    width: 100%;
+    height: 50%;
+    object-fit: scale-down;
+`
+
+const TaskText = styled.div.attrs({ children: 'Find these characters and beat other players highscores!' })`
+    font-family: 'Montserrat', sans-serif;
+    text-align: center;
+    margin: 1em;
 `
 
 function StartGameModal({ onStart, show }) {
     return (
         <Modal show={show}>
             <Container>
+                <CharactersToFindImage />
+                <TaskText />
                 <Button color="success" onClick={onStart}>Start Searching</Button>
             </Container>
         </Modal>
