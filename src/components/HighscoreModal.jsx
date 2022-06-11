89b7c357eb9491a42f182f348d5638dd5f0c249b
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-06-11 23:47:19 +0200

    Create flex wrapper

--- src/components/HighscoreModal.jsx
+++ src/components/HighscoreModal.jsx
@@ -3,6 +3,13 @@ import UploadHighscore from "./UploadHighscore";
 import HighscoreTable from "./HighscoreTable";
  
 import { useState } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
 
 function HighscoreModal({ show, onSubmit, scoreUploaded, highscores }) {
     const [showTable, setShowTable] = useState(false);
@@ -14,7 +21,10 @@ function HighscoreModal({ show, onSubmit, scoreUploaded, highscores }) {
 
     return (
         <Modal show={show}>
-            {showTable && scoreUploaded ? <HighscoreTable /> : <UploadHighscore onSubmit={onNameSubmit} />}
+            <Wrapper>
+                {showTable && scoreUploaded ? <HighscoreTable /> : 
+                                              <UploadHighscore onSubmit={onNameSubmit} />}
+            </Wrapper>
         </Modal>
     );
 }
