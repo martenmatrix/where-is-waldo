991f3d11bdd0ea2f61837addf9b436f75d5d6a9a
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-06-11 20:32:50 +0200

    Add tests for app component

---
+++ src/components/App.test.jsx
@@ -0,0 +1,4 @@
+test.todo('modal disappears after game starts');
+test.todo('logs when a character was found and all characters were found');
+test.todo('displays time elapsed');
+test.todo('shows upload highscore modal after all characters were found');
