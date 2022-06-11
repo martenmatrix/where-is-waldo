a222fe6cac53b3f867489dc8aebafc9ce255cc6f
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-06-11 20:32:23 +0200

    Create App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,13 @@
-import Modal from './Modal';
-import Header from './Header';
-import SearchImage from './SearchImage';
-import Game from '../game';
+import { useAlert } from '@blaumaus/react-alert';
 import { createGlobalStyle } from 'styled-components';
+import { useEffect, useState } from 'react';
+
+import Header from './Header';
+import StartGameModal from './StartGameModal';
+import SearchImage from './SearchImage';
+import HighscoreModal from './HighscoreModal';
+
+import Game from '../game';
 
 const GlobalStyle = createGlobalStyle`
     body {
@@ -13,14 +18,66 @@ const GlobalStyle = createGlobalStyle`
 `
 
 export default function App() {
-    const characters = Game.getCharacters();
+    const [charactersNotFound, setCharactersNotFound] = useState(null);
+    const [showStartModal, setShowStartModal] = useState(true);
+    const [showHighscoreModal, setShowHighscoreModal] = useState(false);
+    const [time, setTime] = useState(null);
+    const alert = useAlert();
+
+    function startGame() {
+        Game.start()
+        setShowStartModal(false);
+    }
+
+    function refreshCharacters() {
+        const charactersNotFound = Game.getCharactersNotFound();
+        setCharactersNotFound(charactersNotFound);
+    }
+
+    function getTime() {
+        const timePassedFormatted = Game.getTimeElapsed();
+        setTime(timePassedFormatted);
+    }
+
+    async function markCharacter(absoluteX, absoluteY, characterId) {
+        const isCorrect = await Game.markCharacterAt(characterId, absoluteX, absoluteY);
+        if (isCorrect) {
+            alert.success("You've found a character");
+            refreshCharacters();
+            const hasWon = Game.hasWon();
+
+            if (hasWon) {
+                alert.success(`You've found all characters in ${Game.getTimeElapsed()}`);
+                setShowHighscoreModal(true);
+            }
+        } else {
+            alert.error("Incorrect");
+        }
+    }
+
+    async function uploadHighscore(name) {
+        await Game.uploadHighscore(name);
+    }
+
+    useEffect(() => {
+        refreshCharacters();
+    }, []);
+
+    useEffect(() => {
+        getTime();
+        const interval = setInterval(getTime, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <>
             <GlobalStyle />
-            <Header time={'5:11'}/>
-            <SearchImage charactersToFind={characters}/>
-            <Modal isActive={false}/>
+
+            <Header time={time}/>
+            <StartGameModal onStart={startGame} show={showStartModal}/>
+            <SearchImage charactersToFind={charactersNotFound} onCharacterSelect={markCharacter}/>
+            <HighscoreModal onSubmit={uploadHighscore} show={showHighscoreModal}/>
         </>
     )
 }
